d3278277b175f8435d9557bdb555f11d4e2d99c0
Shailesh <127823681+ShaileshBeloseT5@users.noreply.github.com>
2024-02-28 16:08:32 +0530

    ft: Login, dashboard and layout components added as needed.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+{
+    path:'', redirectTo:'login',pathMatch:'full'
+},
+{
+
+    path:'',
+    component:LoginComponent
+},
+{
+    path:'',
+    component:LayoutComponent,
+    children:[
+        {
+            path:'dashboard',
+            component:DashboardComponent
+        }
+    ]
+}
+
+];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,49 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { v4 as uuid4} from 'uuid';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
 
+  loginObj: Login;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.loginObj = new Login();
+  }
+
+  onLogin() {
+    debugger;
+    console.log;
+    this.http.post('http://172.17.2.113:8081/LEWServiceV2/api/userManagement', this.loginObj).subscribe((res: any) => {
+      if (res.Response == 'true') {
+        alert("Login Success!");
+      }
+      else { alert(res.Response); }
+    })
+
+  }
+}
+
+export class Login {
+  UserToken: string;
+  Username: string;
+  Password: string;
+  ErrorMessage: string;
+  RequestType:string;
+
+  constructor() {
+    this.Username = '';
+    this.Password = '';
+    this.ErrorMessage = '';
+    this.RequestType='Login';
+    this.UserToken=uuid4();
+  }
 }
